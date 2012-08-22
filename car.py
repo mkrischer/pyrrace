6c799259c9539f0b7385418aa73d4e0a8618cfec
Manuel Krischer <manuel.krischer@gmail.com>
2012-08-22 19:00:04 +0200

    fixed beta

--- car.py
+++ car.py
@@ -47,7 +47,7 @@ car.fill(bkcol)
 car.set_colorkey(bkcol)
 
 #the statusfield
-statusfield = pygame.Surface((350,120))
+statusfield = pygame.Surface((350,140))
 statusfield.fill((200,200,200))
 
 
@@ -98,10 +98,17 @@ laps = 0		#laps gone
 lasttime = 0		#last lap
 fasttime = 0		#fastest lap
 time = 0		#timer
+racetime = 0		#total race time
 
 checkpoint1 = True	#checkpoint on the road (default: True, for start)
 checkpoint2 = True	#checkpoint on the road (default: True, for start)
 newlap = False 		#don't add a new lap if checkpoints aren't flagged true
+
+
+msg=""			#start with empty message
+msgtime=2000		#how long (in ms) message appears
+msgtimer=0		#timemessage started
+
 
 pygame.key.set_repeat(reaction, 25)	#keyboard multiply strokerate
 
@@ -120,37 +127,43 @@ xfont = pygame.font.Font("lcdb.ttf", 20)
 #msg
 msgfont = pygame.font.Font(None, 60)
 
-while True:
-        #print "car at", carx, cary
-
+run = True
+#game loop
+while run:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			run = False
 			break
+		#check for key press
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_ESCAPE:
+				print "leave game"
 				run = False
-				break
+				
+			#steer left			
 			if event.key == pygame.K_LEFT:			  
 				angle = angle - 5
 				if angle == -5:
 					angle = 355
 				car = images[angle]
+			#steer right
 			if event.key == pygame.K_RIGHT:
 				angle = angle + 5
 				if angle == 360:
 					angle = 0
 				car = images[angle]
+			#accelerate
 			if event.key == pygame.K_UP:
 				if speed + acc < max_speed:
 					speed = speed + acc
 				else:
 					speed = max_speed
+			#brake/backward
 			if event.key == pygame.K_DOWN:
 				if speed > 0 and speed - brake < 0:
 					speed = 0
 				if speed <= 0 :
-					speed = speed - brake/5
+					speed = speed - brake/2
 					if speed <  max_backward:
 						speed = max_backward
 				else: 
@@ -162,45 +175,48 @@ while True:
             #            speed = 0
 
 
-	#check for damage
+	#check for damage level
 	if damage >= max_damage:
 		msg = "CAR BROKEN"
-		msgtext = msgfont.render(msg, True, (255,0,0), (0,0,0))
-		screen.blit(msgtext, (screenx/3, screeny/2-25))
-		break	
-
-	# crashing the walls? the outer edges
+                msgtimer = time
+		max_speed = max_speed/4
+		break
+	# crashing the walls?
 	if carx <= 0:
 		carx  = 0 + bumper
 		crash = True
-	elif carx >= screenx - (car_width/2):
+	if carx >= screenx - (car_width/2):
 		carx = screenx - (car_width/2) - bumper
 		crash = True
-        elif cary <= 0:
+        if cary <= 0:
 		cary = 0 + bumper
 		crash = True
-	elif cary >= screeny - (car_width/2) - tribune:
+	if cary >= screeny - (car_width/2) - tribune:
 		cary = screeny - (car_width/2) - tribune - bumper
 		crash = True
+
+	#ok, what to do if crashed
 	if crash:
-		speed = speed - crash_brake
-		damage += speed/max_speed*crash_damage
-		crash = False
+		speed = speed - crash_brake #reduce speed
+		damage += speed/max_speed*crash_damage #increase damage according to speed
+		crash = False #reset flag
+		msg = "Crash"
+		msgtimer = time
 	
 	#mark area for checkpoint 1 
-	if cary > 25 and cary < 131:
+	if cary > 25 and cary < 135:
 		if carx > 980 and carx < 1000 and checkpoint1 == False and newlap:
 			checkpoint1 = True
-			print "hit checkpoint 1"
+			#print "hit checkpoint 1"
 	
 	#mark area for checkpoint 2
 	if carx > 45 and carx < 180:
 		if cary > 350 and cary < 370 and checkpoint2 == False and checkpoint1 and newlap:
 			checkpoint2 = True
 			newlap = False
-			print "hit checkpoint 2"
+			#print "hit checkpoint 2"
 
-	#initial lap count
+	#start/finish line / lap count
 	if cary > 635 and cary < 735:
 		if carx > 582 and carx < 600 and newlap == False:
 			checkpoint1 = False	#reset checkpoints
@@ -208,12 +224,16 @@ while True:
 			newlap = True		#tell checkpoints we have a new lap
 			laps = laps + 1		#inc lap no
 			lasttime = time		#save time
+			racetime = racetime + lasttime # save complete time
 			time = 0		#reset time
 			#check if we have a fastest lap
-			if lasttime > 0 and lasttime < fasttime or fasttime == 0:
-				print "New time record"
-				fasttime = lasttime
-			print "lap" + str(laps)
+			if laps > 1:
+				if lasttime > 0 and lasttime < fasttime or fasttime == 0:
+					#print "New time record"
+					msg = "New Record!"
+					msgtimer = time
+					fasttime = lasttime
+			#print "lap" + str(laps)
 
         # calculate speed according to driving angle
 	movx = speed * math.cos(math.radians(angle))
@@ -252,34 +272,46 @@ while True:
 	#render status
 	statustext = statusfont.render(status, True, (255,255,0))
 	
+	#reset message after msgtime ticks
+	if time >  msgtimer + msgtime:
+		msg = ""
+
 	#race clock
 	if laps > 0:
 		time = time + clock.get_time() 
 	
+	lcout  = str(laps) + ". Runde"
 	timer1 = "Letze Runde:  " + str(round(lasttime/float(1000),3))
 	timer2 = "Schnellste Runde:  " + str(round(fasttime/float(1000),3))
 	timer3 = "Aktuelle Zeit:  " + str(round(time/float(1000),3))
-	damage = "Schaden:  " + str(damage/max_damage*100) + "/" + str(max_damage*10)
+	dmg = "Schaden:  " + str(damage/max_damage*100) + "/" + str(max_damage*10)
 
+	lc  = xfont.render(lcout, True, (0,0,0), (200,200,200)) #laps driven
 	xt1 = xfont.render(timer1, True, (0,0,0), (200,200,200)) #lastlap
 	xt2 = xfont.render(timer2, True, (0,0,0), (200,200,200)) #fastestlap
 	xt3 = xfont.render(timer3, True, (0,0,0), (200,200,200)) #acttime
-	xt4 = xfont.render(damage, True, (0,0,0), (200,200,200)) #car damage
-	
+	xt4 = xfont.render(dmg, True, (0,0,0), (200,200,200)) #car damage
+	msgtext = msgfont.render(msg, True, (255,0,0), (0,0,0))
+
 	#paint all stuff
         screen.blit(racefield,[0,0])
-	screen.blit(statusfield,((screenx/2)-300, screeny-300))	
-        screen.blit(speedtext, (screenx/2-100, screeny-225))        
-	screen.blit(xt1, (screenx/2-290, screeny-295))
-	screen.blit(xt2, (screenx/2-290, screeny-270))
-	screen.blit(xt3, (screenx/2-290, screeny-245))
-	screen.blit(xt4, (screenx/2-290, screeny-220))
+	screen.blit(statusfield,((screenx/2)-330, screeny-320))	
+        screen.blit(speedtext, (screenx/2-120, screeny-310))
+	screen.blit(lc, (screenx/2-320, screeny-310))
+	screen.blit(xt1, (screenx/2-320, screeny-285))
+	screen.blit(xt2, (screenx/2-320, screeny-260))
+	screen.blit(xt3, (screenx/2-320, screeny-235))
+	screen.blit(xt4, (screenx/2-320, screeny-210))
 	screen.blit(car,[carx-35,cary-35])
+	screen.blit(msgtext, (screenx/3, screeny/2-25))
 
+	
 	if debug:
 		screen.blit(carpostext, (50, screeny-50))
 		screen.blit(statustext, (50, 50))
 
 	pygame.display.update();
 	clock.tick(100)
+print "you needed " + str(racetime/1000/60) + ":" + str(racetime/1000%60) + ":" + str(racetime/1000/60%60) + " min for " + str(laps) + " laps "
+print "your fastest lap was " + str(round(fasttime/float(1000),3)) + " seconds"
 pygame.quit()
